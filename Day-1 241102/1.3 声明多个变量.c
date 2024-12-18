b765a421c433fc22de37ec94a5b6779eac2b846a
Fei Mo <mofei263@outlook.com>
2024-12-18 11:51:35 -0500

    rearranged order

--- Day-1 241102/1.3 声明多个变量.c
+++
@@ -1,9 +0,0 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int x = 1, y = 2, z = 3;
-//	printf("%d", x + y + z);
-//	return 0;
-//}
--- Day-1 241102/2.1 常量.c
+++
@@ -1,8 +0,0 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//int main()
-//{
-//	const MESSI = 10;
-//	printf("%d", MESSI);
-//	return 0;
-//}
