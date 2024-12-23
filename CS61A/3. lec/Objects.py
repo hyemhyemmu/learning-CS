f93fa3e36d7169468d2eaf74f5a165a87a9b6d27
Fei Mo <mofei263@outlook.com>
2024-12-23 15:45:29 -0500

    lec: Objects - Define Class

---
+++ CS61A/3. lec/Objects.py
@@ -0,0 +1,16 @@
+# class
+class Account:
+    def __init__(self, account_holder):
+        # define attributes
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+        return self.balance
+
+    def withdraw(self,amount):
+        if amount>self.balance:
+            return 'Insufficient funds'
+        self.balance=self.balance-amount
+        return self.balance
