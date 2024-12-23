34a5f6a02982a1ad27b15ad371137df88ff51b49
Fei Mo <mofei263@outlook.com>
2024-12-23 09:44:30 -0500

    lec: Generators

--- CS61A/3. lec/Generators.py
+++ CS61A/3. lec/Generators.py
@@ -18,7 +18,7 @@ def count_partitions(n, m):
 # list partitions
 def list_partitions(n, m):
     """list partitions
-    >>>list_partitions(6,4)
+    >>>for t in list_partitions(6,4):print(t)
     [4, 2]
     [4, 1, 1]
     [3, 3]
@@ -49,7 +49,7 @@ def list_partitions(n, m):
 # string version
 def partitions(n, m):
     """list partitions
-    >>>partitions(6,4)
+    >>>for t in partitions(6,4):print(t)
     2+4
     1+1+4
     3+3
@@ -77,7 +77,7 @@ def partitions(n, m):
 # yield version
 def yield_partitions(n, m):
     """list partitions
-    >>>yield_partitions(6,4)
+    >>>for t in yield_partitions(6,4):print(t)
     2+4
     1+1+4
     3+3
@@ -96,3 +96,75 @@ def yield_partitions(n, m):
         for p in yield_partitions(n - m, m):
             yield p + '+' + str(m)
         yield from yield_partitions(n, m - 1)
+
+
+# fib generator
+def fib_generator():
+    """
+    A generator that yields the Fibonacci sequence indefinitely.
+    (The Fibonacci sequence starts with 0 and 1, and each subsequent number
+    is the sum of the previous two.)
+    >>> fib = fib_generator()
+    >>> next(fib)
+    0
+    >>> next(fib)
+    1
+    >>> next(fib)
+    1
+    >>> next(fib)
+    2
+    >>> list(next(fib) for i in range(0,10)) # list the next 10 fibonacci numbers
+    [3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+    """
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+# iterator practice
+def add_to_each(p, edit):
+    """
+    Given a list, p, of 3-element tuples: [(x1, y1, z1), (x2, y2, z2), ...]
+    And an edit tuple (also 3 elements) = (a, b, c),
+    return a map object where
+    a is added to each x-value,
+    b to each y-value, and
+    c to each z-value.
+    >>> list(add_to_each([(0, 0, 0), (1, 1, 1)], (10, 10, 10)))
+    [(10, 10, 10), (11, 11, 11)]
+    >>> list(add_to_each([(1, 2, 3), (1, 1, 1)], (10, 20, 30)))
+    [(11, 22, 33), (11, 21, 31)]
+    """
+    return map(lambda x: (x[0] + edit[0], x[1] + edit[1], x[2] + edit[2]), p)
+
+
+# tree practice
+# 2023 MT2 Q4(a)
+from Trees import *
+
+
+def exclude(t, x):
+    """
+    Return a Tree with the non-root nodes of t whose labels are not equal to x.
+
+    >>> t = tree(1, [tree(2, [tree(2), tree(3)]), tree(4, [tree(1)])])
+    >>> exclude(t, 2)
+    tree(1, [tree(3), tree(4, [tree(1)])])
+
+    >>> t
+    # t is not changed
+    tree(1, [tree(2, [tree(2), tree(3)]), tree(4, [tree(1)])])
+
+    >>> exclude(t, 1) # The root node cannot be excluded
+    tree(1, [tree(2, [tree(2), tree(3)]), tree(4)])
+    """
+    filtered_branches = map(lambda y: exclude(y, x), t.branches)
+
+    bs = []
+    for b in filtered_branches:
+        if label(b) == x:
+            bs.extend(branches(b))
+        else:
+            bs.append(b)
+    return tree(t.label, bs)
