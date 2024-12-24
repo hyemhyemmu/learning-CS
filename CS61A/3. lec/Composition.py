d1f87083db7e6d74b98feffc3a6febf107beb0b2
Fei Mo <mofei263@outlook.com>
2024-12-24 16:00:18 -0500

    lec: Composition - Linked Lists

--- CS61A/3. lec/Composition.py
+++ CS61A/3. lec/Composition.py
@@ -42,7 +42,7 @@ def add(s, v):
     assert s is not Link.empty
     # very beginning
     if s.first > v:
-        s.first,s.rest=v,Link(s.first,s.rest)
+        s.first, s.rest = v, Link(s.first, s.rest)
 
     # centre
     elif s.first < v:
@@ -52,3 +52,36 @@ def add(s, v):
     elif s.first < v and s.rest == Link.empty:
         s.rest = Link(v)
     return s
+
+
+# slicing a linked list
+def slice_link(s, i, j):
+    """
+    Return a linked list containing elements from i:j
+    """
+    if s == Link.empty or j == 0:
+        return Link.empty
+    elif i == 0:
+        return Link(s.first, slice_link(s.rest, i, j - 1))
+    else:  # i!=0
+        return slice_link(s.rest, i - 1, j - 1)
+
+
+# Inserting into a Linked List
+def insert_link(s, x, i):
+    """
+    Insert x into linked list s at index i.
+    """
+    if s is Link.empty:
+        print('Index out of range')
+    # beginning
+    elif i == 0:
+        original = Link(s.first, s.rest)
+        s.first = x
+        s.rest = original
+    # back
+    elif i == 1 and s.rest is Link.empty:
+        s.rest = Link(x)
+    # centre ->recursion
+    else:
+        insert_link(s.rest, x, i - 1)
