9a29059045831e396e588b68e8c42a71a43d2f82
Fei Mo <mofei263@outlook.com>
2024-12-25 16:19:10 -0500

    disc08: Linked Lists

---
+++ CS61A/5. disc/disc08.py
@@ -0,0 +1,190 @@
+# Topic: Linked Lists
+# Q1: strange loop
+def strange_loop():
+    """Return a Link s for which s.rest.first.rest is s.
+
+    >>> s = strange_loop()
+    >>> s.rest.first.rest is s
+    True
+    """
+    s = Link(10000000, Link(Link(1)))
+    s.rest.first.rest = s
+    return s
+
+
+# Q2: Sum Two Ways
+def sum_rec(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_rec(a, 2)
+    7
+    >>> sum_rec(a, 5)
+    15
+    >>> sum_rec(Link.empty, 1)
+    0
+    """
+    # Use a recursive call to sum_rec; don't call sum_iter
+    result = 0
+    if s is Link.empty or k == 0:
+        return result
+    else:
+        result = result + s.first + sum_rec(s.rest, k - 1)
+        return result
+
+
+def sum_iter(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_iter(a, 2)
+    7
+    >>> sum_iter(a, 5)
+    15
+    >>> sum_iter(Link.empty, 1)
+    0
+    """
+    # Don't call sum_rec or sum_iter
+    result = 0
+    while s is not Link.empty and k != 0:
+        result = result + s.first
+        s = s.rest
+        k = k - 1
+    return result
+
+
+# Q3: Overlap
+def overlap(s, t):
+    """For increasing s and t, count the numbers that appear in both.
+
+    >>> a = Link(3, Link(4, Link(6, Link(7, Link(9, Link(10))))))
+    >>> b = Link(1, Link(3, Link(5, Link(7, Link(8)))))
+    >>> overlap(a, b)  # 3 and 7
+    2
+    >>> overlap(a.rest, b)  # just 7
+    1
+    >>> overlap(Link(0, a), Link(0, b))
+    3
+    """
+    if s is Link.empty or t is Link.empty:
+        return 0
+    elif s.first == t.first:
+        return 1 + overlap(s.rest, t.rest)
+    elif s.first < t.first:
+        return overlap(s.rest, t)
+    else:
+        return overlap(s, t.rest)
+
+
+def overlap_iter(s, t):
+    """For increasing s and t, count the numbers that appear in both.
+
+    >>> a = Link(3, Link(4, Link(6, Link(7, Link(9, Link(10))))))
+    >>> b = Link(1, Link(3, Link(5, Link(7, Link(8)))))
+    >>> overlap_iter(a, b)  # 3 and 7
+    2
+    >>> overlap_iter(a.rest, b)  # just 7
+    1
+    >>> overlap_iter(Link(0, a), Link(0, b))
+    3
+    """
+    count = 0
+    while s is not Link.empty and t is not Link.empty:
+        if s.first == t.first:
+            count += 1
+            s, t = s.rest, t.rest
+        elif s.first < t.first:
+            s = s.rest
+        else:
+            t = t.rest
+    return count
+
+
+# Extra Challenge
+# Decimal Expansion
+def divide(n, d):
+    """Return a linked list with a cycle containing the digits of n/d.
+
+    >>> display(divide(5, 6))
+    0.8333333333...
+    >>> display(divide(2, 7))
+    0.2857142857...
+    >>> display(divide(1, 2500))
+    0.0004000000...
+    >>> display(divide(3, 11))
+    0.2727272727...
+    >>> display(divide(3, 99))
+    0.0303030303...
+    >>> display(divide(2, 31), 50)
+    0.06451612903225806451612903225806451612903225806451...
+    """
+    assert n > 0 and n < d
+    result = Link(0)  # The zero before the decimal point
+    cache = {}
+    # use a pointer, so that it is easier to track and understand
+    p = result
+
+    while n not in cache:
+        # quotient and remainder
+        q, r = (10 * n) // d, (10 * n) % d
+        p.rest = Link(q)
+        p = p.rest
+        cache[n] = p  # store current remainder and point
+        n = r
+    p.rest = cache[n]
+
+    return result
+
+
+def display(s, k = 10):
+    """Print the first k digits of infinite linked list s as a decimal.
+
+    >>> s = Link(0, Link(8, Link(3)))
+    >>> s.rest.rest.rest = s.rest.rest
+    >>> display(s)
+    0.8333333333...
+    """
+    assert s.first == 0, f'{s.first} is not 0'
+    digits = f'{s.first}.'
+    s = s.rest
+    for _ in range(k):
+        assert s.first >= 0 and s.first < 10, f'{s.first} is not a digit'
+        digits += str(s.first)
+        s = s.rest
+    print(digits + '...')
+
+
+# Link Class
+class Link:
+    """A linked list is either a Link object or Link.empty
+
+    >>> s = Link(3, Link(4, Link(5)))
+    >>> s.rest
+    Link(4, Link(5))
+    >>> s.rest.rest.rest is Link.empty
+    True
+    >>> s.rest.first * 2
+    8
+    >>> print(s)
+    <3 4 5>
+    """
+    empty = ()
+
+    def __init__(self, first, rest = empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
