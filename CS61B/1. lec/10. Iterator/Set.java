d124ae9d7d56b2520fa0c800c08b3f75401f6247
Fei Mo <mofei263@outlook.com>
2024-12-31 10:12:17 -0500

    lec10: Iterator

---
+++ CS61B/1. lec/10. Iterator/Set.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+
+public class Set<Integer> {
+    public static Set<Integer> javaset;
+
+    public Set(){
+
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> javaset=new Set<>();
+    }
+}
