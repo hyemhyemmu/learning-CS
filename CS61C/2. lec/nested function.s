46db87766168fc6072f4a9f7f56b24df601a1f8f
Daniel <mofei263@outlook.com>
2025-01-16 20:21:32 -0500

    RSIV-C syntax practice

--- CS61C/2. lec/nested function.s
+++ CS61C/2. lec/nested function.s
@@ -9,9 +9,14 @@ mult:
 sumSquare:
     addi sp,sp,-8 //分配内存
     sw ra,4(sp)  //存放下一条指令的地址，防止被覆盖
-    sw a0,0(sp)
+    sw a0,0(sp) //把原来a0里的数据存放起来，因为后面会被修改
 
     mv a1,a0
     jal mult //调用mult，结果存储在a0
 
-    lw a0,0(sp)
+    lw a0,0(sp) //恢复a0的数据
+    add a0,a0,a1
+
+    lw ra,4(sp) //大函数的地址
+    addi sp,sp,8
+    jr ra
