2c66fa94bbfc62030e3c3216c26e921e9c1cc013
hyemhyemmu <mofei263@outlook.com>
2025-04-07 16:50:12 -0400

    stuff

--- One Student One Chip/1. Pre-Request/LinkedList.c
+++ One Student One Chip/1. Pre-Request/LinkedList.c
@@ -7,3 +7,43 @@ List *List_create(){
     else
         return newNode;
 }
+
+void List_destroy(List *list){
+    LIST_FOREACH(list, first, next, curr){
+        if (curr->prev){
+            free(curr->prev);
+        }
+    }
+    free(list->last);
+    free(list);
+}
+
+
+void List_clear(List *list){
+    LIST_FOREACH(list, first, next, curr){
+        free(curr->next);
+    }
+}
+
+void List_clear_destroy(List *list){
+    List_clear(list);
+    List_destroy(list);
+}
+
+void List_push(List *list, void *value){
+    // 一直遍历到末尾
+    LIST_FOREACH(list, first, next, curr){
+        if (!curr->next){
+            ListNode *newNode = calloc(1, sizeof(List));
+            if (newNode == NULL)
+                printf(errno);
+            else{
+                newNode->value = value;
+                newNode->prev = curr;
+                newNode->next = NULL;
+                curr->next = newNode;
+                list->count++;
+            }
+        }
+    }
+}
